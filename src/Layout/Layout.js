2c6ad335a505145a2c0df86e30ea6d5a74b9e3c7
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-07 12:07:09 +0200

    added layout and navbar components

---
+++ src/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React
+    from 'react';
+
+const layout = (props) => {
+    return (
+        <div>
+            Layout
+            {props.children}
+        </div>
+    );
+};
+
+export default layout;
