971a3d40ea91de54a824b44f7586ea0cf07e67f3
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-09 08:19:19 +0200

    added structure to lessons page, navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Layout from './Layout/Layout';
 import Auth from './Auth/Auth';
+import Lessons from './Lessons/Lessons';
 
 function App() {
     return (
@@ -12,7 +13,7 @@ function App() {
             <Layout>
                 <Switch>
                     <Route path='/auth' component={Auth} />
-                    <Route path='/lessons' component={""} />
+                    <Route path='/lessons' component={Lessons} />
                     <Route path="/" exact component={""}/>
                     <Redirect to="/auth"/>
                 </Switch>
---
+++ src/Lessons/Lessons.js
@@ -0,0 +1,12 @@
+import React
+    from 'react';
+
+const lessons = () => {
+    return (
+        <div>
+            lessons
+        </div>
+    );
+};
+
+export default lessons;
