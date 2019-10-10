44a740de85b1da4814e338fb972aa7782dd51e91
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-10 14:49:03 +0200

    added onboarding checklist to panel

--- src/Layout/Header/OnboardingPanel/OnBoardingPanel.js
+++ src/Layout/Header/OnboardingPanel/OnBoardingPanel.js
@@ -1,6 +1,8 @@
 import React
     from 'react';
-import { Badge } from 'react-bootstrap';
+import { Badge, ListGroup } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare, faSquare } from '@fortawesome/free-solid-svg-icons';
 
 import classes from './OnBoardingPanel.module.css';
 
@@ -15,12 +17,26 @@ const onBoardingPanel = () => {
             </div>
             <p><strong>Level up:</strong></p>
             <div className={classes.ProgressBar}>
-                <div
-                    className={classes.ProgressLevel}>
-
+                <div className={classes.ProgressLevel}>
                 </div>
             </div>
-
+            <ListGroup>
+                <ListGroup.Item>
+                    <FontAwesomeIcon icon={faCheckSquare} /> Submit your first assignment.
+                    <div className={classes.Points}><Badge variant="secondary">10 points</Badge></div></ListGroup.Item>
+                <ListGroup.Item>
+                    <FontAwesomeIcon icon={faCheckSquare} /> Submit at least five assignments.
+                    <div className={classes.Points}><Badge variant="secondary">20 points</Badge></div></ListGroup.Item>
+                <ListGroup.Item>
+                    <FontAwesomeIcon icon={faCheckSquare} /> Register to the community.
+                    <div className={classes.Points}><Badge variant="secondary">30 points</Badge></div></ListGroup.Item>
+                <ListGroup.Item>
+                    <FontAwesomeIcon icon={faCheckSquare} /> Finish your first project.
+                    <div className={classes.Points}><Badge variant="secondary">40 points</Badge></div></ListGroup.Item>
+                <ListGroup.Item>
+                    <FontAwesomeIcon icon={faSquare} /> Invite a friend.
+                    <div className={classes.Points}><Badge variant="secondary">50 points</Badge></div></ListGroup.Item>
+            </ListGroup>
         </div>
     );
 };
