f4aca83412b1b91594850dbcdc5da2a859b81ca9
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-10 13:56:40 +0200

    changed double quote to single ones

--- src/Auth/LoginForm/LoginForm.js
+++ src/Auth/LoginForm/LoginForm.js
@@ -19,16 +19,16 @@ const loginForm = (props) => {
     return (
         <React.Fragment>
             <form
-                action="/lessons">
+                action={'/lessons'}>
                 <FormGroup>
                     <InputGroup>
                         <InputGroup.Prepend>
                             <InputGroup.Text><FontAwesomeIcon icon={faUser} /></InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl
-                            type="email"
-                            name="email"
-                            placeholder="my@email.com" />
+                            type={'email'}
+                            name={'email'}
+                            placeholder={'my@email.com'} />
                     </InputGroup>
                 </FormGroup>
                 <FormGroup>
@@ -37,19 +37,19 @@ const loginForm = (props) => {
                             <InputGroup.Text><FontAwesomeIcon icon={faLock} /></InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl
-                            type="password"
-                            name="password"
-                            placeholder="MyPassword" />
+                            type={'password'}
+                            name={'password'}
+                            placeholder={'MyPassword'} />
                     </InputGroup>
                 </FormGroup>
 
-                <Button onClick={props.signedIn} variant={"primary"} className={[classes.btnBlock, classes.btnPrimary].join(' ')}>Log in <FontAwesomeIcon icon={faSignInAlt} /></Button>
+                <Button onClick={props.signedIn} variant={'primary'} className={[classes.btnBlock, classes.btnPrimary].join(' ')}>Log in <FontAwesomeIcon icon={faSignInAlt} /></Button>
             </form>
-            <p className="text-right text-muted">
-                <small><a href="/auth/password-reset">Forgot your password?</a></small>
+            <p className={'text-right text-muted'}>
+                <small><a href={'/auth/password-reset'}>Forgot your password?</a></small>
             </p>
-            <p className="text-muted">
-                <small>Don't have an account? <a href="/auth/signup"> Sign up now</a>!</small>
+            <p className={'text-muted'}>
+                <small>Don't have an account? <a href={'/auth/signup'}> Sign up now</a>!</small>
             </p>
         </React.Fragment>
     );
--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -12,44 +12,44 @@ const header = (props) => {
     return (
         <header className={classes.HeaderFixedTop}>
             <Navbar
-                expand="lg">
-                <Navbar.Brand href="/"><img
-                    alt="Logo"
-                    src="/images/logo/logo-60x60.png" />
+                expand={'lf'}>
+                <Navbar.Brand href={'/'}><img
+                    alt={'Logo'}
+                    src={'/images/logo/logo-60x60.png'} />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="fixed-navbar-nav" />
-                <Navbar.Collapse id="fixed-navbar-nav">
-                    <Nav className="mr-auto navigation" >
-                        <Nav.Link href="/service/slack/invite/en-US">Community</Nav.Link>
-                        <NavDropdown title="Admin menu" id="admin-nav-dropdown">
-                            <NavDropdown.Item href="/user/search">Students</NavDropdown.Item>
-                            <NavDropdown.Item href="/curriculum-repository/projects">Lessons</NavDropdown.Item>
-                            <NavDropdown.Item href="/curriculum-repository/concepts">Concepts</NavDropdown.Item>
-                            <NavDropdown.Item href="/reference">Reference</NavDropdown.Item>
-                            <NavDropdown.Item href="/stats">Statistics</NavDropdown.Item>
-                            <NavDropdown.Item href="/curriculum-repository/processes">Curriculum i18n tools</NavDropdown.Item>
-                            <NavDropdown.Item href="/i18n/processes">Framework i18n tools</NavDropdown.Item>
-                            <NavDropdown.Item href="/test-tools">Test tools</NavDropdown.Item>
+                <Navbar.Toggle aria-controls={'fixed-navbar-nav'} />
+                <Navbar.Collapse id={'fixed-navbar-nav'}>
+                    <Nav className={'mr-auto navigation'} >
+                        <Nav.Link href={'/service/slack/invite/en-US'}>Community</Nav.Link>
+                        <NavDropdown title={'Admin menu'} id={'admin-nav-dropdown'}>
+                            <NavDropdown.Item href={'/user/search'}>Students</NavDropdown.Item>
+                            <NavDropdown.Item href={'/curriculum-repository/projects'}>Lessons</NavDropdown.Item>
+                            <NavDropdown.Item href={'/curriculum-repository/concepts'}>Concepts</NavDropdown.Item>
+                            <NavDropdown.Item href={'/reference'}>Reference</NavDropdown.Item>
+                            <NavDropdown.Item href={'/stats'}>Statistics</NavDropdown.Item>
+                            <NavDropdown.Item href={'/curriculum-repository/processes'}>Curriculum i18n tools</NavDropdown.Item>
+                            <NavDropdown.Item href={'/i18n/processes'}>Framework i18n tools</NavDropdown.Item>
+                            <NavDropdown.Item href={'/test-tools'}>Test tools</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <Nav className="mr-sm-2">
+                    <Nav className={'mr-sm-2'}>
                         <Nav.Link
-                            href="/user/referral">
+                            href={'/user/referral'}>
                             <img
-                                alt="Gift"
+                                alt={'Gift'}
                                 className={classes.GiftImg}
-                                src="/images/referral-icon.png" />
+                                src={'/images/referral-icon.png'} />
                         </Nav.Link>
                         <Nav.Link>
                             <NavDropdown
                                 alignRight
                                 title={props.user.userInitials}
                                 className={classes.UserMenu}
-                                id="user-nav-dropdown">
-                                <NavDropdown.Item href="/user/search"><OnBoardingPanel /></NavDropdown.Item>
+                                id={'user-nav-dropdown'}>
+                                <NavDropdown.Item href={'/user/search'}><OnBoardingPanel /></NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="/user/profile"><FontAwesomeIcon icon={faUser} /> Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="/auth/logout"><FontAwesomeIcon icon={faSignOutAlt} /> Logout</NavDropdown.Item>
+                                <NavDropdown.Item href={'/user/profile'}><FontAwesomeIcon icon={faUser} /> Profile</NavDropdown.Item>
+                                <NavDropdown.Item href={'/auth/logout'}><FontAwesomeIcon icon={faSignOutAlt} /> Logout</NavDropdown.Item>
                             </NavDropdown>
                         </Nav.Link>
                     </Nav>
