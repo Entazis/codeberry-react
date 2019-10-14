e4458cc59088b66c30502c8ca568128bd9eadabd
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-14 16:31:06 +0200

    navigation css fixes

--- src/Lessons/Navigation/Navigation.js
+++ src/Lessons/Navigation/Navigation.js
@@ -5,24 +5,29 @@ import {useWindowSize} from '../../hooks/useWindowSize';
 const Navigation = () => {
     const [moduleTitle, setModuleTitle] = useState('Welcome to CodeBerry');
     const [moduleProgress, setModuleProgress] = useState('100%');
-    const [lessonTitle, setLessonTitle] = useState('Welcome to CodeBerry');
+    const [lessonTitle, setLessonTitle] = useState('Welcome to CodeBerry!');
     const windowSize = useWindowSize();
 
     const controllerClasses = [classes.Controller];
+    const thumbnailClasses = [classes.Thumbnail];
+    const headerClasses = [classes.Header];
     if (windowSize.width < 1360) {
-        controllerClasses.push(classes.BottomFix);
+        controllerClasses.push(classes.ControllerBottom);
+        thumbnailClasses.push(classes.ThumbnailBottom);
+        headerClasses.push(classes.HeaderBottom)
     }
 
     return (
         <div className={controllerClasses.join(' ')}>
-            <div className={classes.Header}>
+            <div className={headerClasses.join(' ')}>
                 <div className={classes.Titles}>
-                    <h1 title={moduleTitle}>{moduleTitle} <small className="text-muted">({moduleProgress})</small></h1>
-                    <p>{lessonTitle}</p>
+                    <h1 className={classes.ModuleTitle} title={moduleTitle}>{moduleTitle} <small className="text-muted">({moduleProgress})</small></h1>
+                    <p className={classes.LessonTitle}>{lessonTitle}</p>
                 </div>
-                <div className={classes.Thumbnail}>
-                    <img src="/logo192.png" alt="Project thumbnail" />
+                <div className={thumbnailClasses.join(' ')}>
+                    <img src="/logo192.png" alt="Project thumbnail" className={classes.ThumbnailImg} />
                 </div>
+
             </div>
             <div className={classes.Body}>...</div>
         </div>
