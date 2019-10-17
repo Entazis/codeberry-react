439ecc73ec7c7533fc8d6f8839553c568b043a33
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-17 11:52:48 +0200

    merged card into accordion

--- src/Lessons/Navigation/Navigation.js
+++ src/Lessons/Navigation/Navigation.js
@@ -31,25 +31,24 @@ const Navigation = () => {
 
     return (
         <div className={controllerClasses.join(' ')}>
-            <Accordion defaultActiveKey='0'>
-                <Card>
+            <Accordion as={Card} defaultActiveKey='0'>
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
                     <div className={classes.ControllerHeader}>
-                        <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
-                            <div className={classes.Titles}>
-                                <h1 className={classes.ModuleTitle} title={selectedModule.title}>{selectedModule.title} <small className="text-muted">({selectedModule.progress})</small></h1>
-                                <p className={classes.chapterTitle}>{selectedChapter.title}</p>
-                            </div>
-                            <div className={thumbnailClasses.join(' ')}>
-                                <img src="/logo192.png" alt="Project thumbnail" className={classes.ThumbnailImg} />
-                            </div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="h1">
-                        <Card.Body>
-                            <Modules modules={modules}/>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+                        <div className={classes.Titles}>
+                            <h1 className={classes.ModuleTitle} title={selectedModule.title}>{selectedModule.title} <small className="text-muted">({selectedModule.progress})</small></h1>
+                            <p className={classes.chapterTitle}>{selectedChapter.title}</p>
+                        </div>
+                        <div className={thumbnailClasses.join(' ')}>
+                            <img src="/logo192.png" alt="Project thumbnail" className={classes.ThumbnailImg} />
+                        </div>
+                    </div>
+                </Accordion.Toggle>
+
+                <Accordion.Collapse as={Card.Body} eventKey="1">
+                    <div className={classes.ControllerBody}>
+                        <Modules modules={modules}/>
+                    </div>
+                </Accordion.Collapse>
             </Accordion>
         </div>
     );
