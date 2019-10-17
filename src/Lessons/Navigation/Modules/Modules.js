91cdb28e6c654de0f652727a471482404bb33db7
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-17 16:23:55 +0200

    added module styles and progress

--- src/Lessons/Navigation/Modules/Modules.js
+++ src/Lessons/Navigation/Modules/Modules.js
@@ -3,33 +3,43 @@ import React from 'react';
 import classes from './Modules.module.css';
 import {Accordion, Card} from 'react-bootstrap';
 import Chapters from './Chapters/Chapters';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 
 const modules = (props) => {
     const moduleList = props.modules.map((module, index) =>
         {
             return (
-            <div className={classes.Module} key={module.id}>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} variant="link" eventKey={'m' + index}>
-                        {module.title}
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey={'m' + index}>
-                        <Card.Body>
-                            <Chapters chapters={module.chapters}/>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </div>
-        )
+                <div className={classes.Module}>
+                    <Accordion as={Card} defaultActiveKey="0">
+                        <Accordion.Toggle as={Card.Header} variant="link" eventKey={'m' + index}>
+                            <div className={classes.ModuleHeader}>
+                                <img className={classes.ModuleThumbnail}
+                                     src="/logo192.png"
+                                     alt="Module thumbnail" />
+                                <span className={classes.ModuleTitle}>{module.title}</span>
+                                {module.progress === '100%' ? (
+                                    <FontAwesomeIcon icon={faCheckCircle} className={classes.ProgressDoneRight} />
+                                ) : (
+                                    <span className={classes.ProgressRight} title="Module status">{module.progress}</span>
+                                )}
+                            </div>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse as={Card.Body} eventKey={'m' + index}>
+                            <div className={classes.ModuleBody}>
+                                <Chapters chapters={module.chapters}/>
+                            </div>
+                        </Accordion.Collapse>
+                    </Accordion>
+                </div>
+            )
         }
     );
 
     return (
-        <div className={classes.Modules}>
-            <Accordion defaultActiveKey="0">
-                {moduleList}
-            </Accordion>
-        </div>
+        <React.Fragment>
+            {moduleList}
+        </React.Fragment>
     );
 };
 
