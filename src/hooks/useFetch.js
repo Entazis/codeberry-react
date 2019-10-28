a41a1f2a662b6e112e6b3b4bebac23b2c765f566
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-28 11:44:49 +0100

    moved fetchData into useCallback

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,24 +1,25 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect, useCallback} from 'react';
 
 export default (url, options) => {
     const [response, setResponse] = useState(null);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            try {
-                const res = await fetch(url, options);
-                const json = await res.json();
-                setResponse(json);
-                setIsLoading(false);
-            } catch (error) {
-                setError(error);
-            }
-        };
-        fetchData();
+    const fetchData = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const res = await fetch(url, options);
+            const json = await res.json();
+            setResponse(json);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+        }
     }, [url, options]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
 
     return { response, error, isLoading };
 };
